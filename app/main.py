10e84ad3d81f8801605149bf806b6ef510221fd8
yhd0210 <yhd060210@gmail.com>
2023-09-20 00:49:52 +0900

    main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.config import settings
+from user.models import Base
 from core.config import settings
 from user.user_router import router as user_router
+
+DB_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?charset=utf8"
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, version='1.0.0')
 app.include_router(user_router, prefix="/user", tags=["users"])
@@ -22,4 +34,4 @@ async def test():
     return 'Server is Working!'
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", port=8080,reload=True)
