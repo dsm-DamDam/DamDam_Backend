3765ef8ee106bc386335bbf7775417b273026a10
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:40:39 +0900

    user-schemas

--- app/user/schemas.py
+++ app/user/schemas.py
@@ -1,41 +1,44 @@
-from pydantic import BaseModel, validator, constr
-from fastapi import HTTPException, status
-import re
+from pydantic import BaseModel, constr
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    id : int
+
+class updatePW(BaseModel):
+    id : int
+    password : str
+    change_password : str
+    confirm_change_password : str
+
+class updateInfo(BaseModel):
+    id : int
+    new_nickname : str
+    new_userID : str
+    new_email : str
+    password : str
 
 class VerificationRequest(BaseModel):
     email: constr()
     verification_code: constr()
 
 class User(BaseModel):
-    id: constr()
-    nickname: constr()
-    email: constr()
-    password: constr()
+    userID: str
+    nickname: str
+    email: str
+    password: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class SignUp(BaseModel):
-    nickname: constr()
-    email: constr()
-    account_id: constr()
-    password: constr()
-    confirm_password: constr()
-
-    @validator('password')
-    def check_password(cls, v):
-        REGEX_PASSWORD = r'^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,24}$'
-        if not re.fullmatch(REGEX_PASSWORD, v):
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="비밀번호 형식이 잘못됨")
-        return v
+    auth_code : int
+    nickname: str
+    email: str
+    userID: str
+    password: str
+    confirm_password: str
 
 class Login(BaseModel):
-    account_id: constr()
-    password: constr()
-
-    @validator('password')
-    def check_password(cls, v):
-        REGEX_PASSWORD = r'^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,24}$'
-        if not re.fullmatch(REGEX_PASSWORD, v):
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="비밀번호 형식이 잘못됨")
-        return v
+    userID: str
+    password: str
