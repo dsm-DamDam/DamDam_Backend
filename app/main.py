bf3688e71ce29340abdb1791f07c9e2cc0cc32ac
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:41:27 +0900

    main-file

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ import uvicorn
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
-from user.models import Base
-from core.config import settings
+from database.models import Base
 from user.user_router import router as user_router
+from item.item_router import router as item_router
+
 
 DB_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?charset=utf8"
 
@@ -17,7 +18,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, version='1.0.0')
+
 app.include_router(user_router, prefix="/user", tags=["users"])
+app.include_router(item_router, prefix="/item", tags=["items"] )
 
 origins = ["*"]
 
