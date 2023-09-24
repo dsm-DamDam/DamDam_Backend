8b4f6b4d54333d3c34430edb9546b8185708dfc2
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:42:57 +0900

    DB-model

--- app/database/models.py
+++ app/database/models.py
@@ -1,20 +1,35 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
+
+    userID = Column(String(20), unique = True, index=True)
+    email = Column(String(50), unique=True, index=True)
+    nickname = Column(String(10))
+    password = Column(String(30))
+    #counts = relationship("Count", back_populates="user")
     #is_verified = Column(Boolean, default=False)
     #verification_code = Column(String, nullable=True)
-
+    
 class Count(Base):
     __tablename__ = "counts"
 
-    id = Column(Integer, primary_key=True)
-    day = Column(String)
-    cnt = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True,autoincrement=True)
+    #userID = Column(Integer, ForeignKey("users.id"))
+    date = Column(Date)
+    count = Column(Integer)
+
+    #user = relationship("User", back_populates="counts")
+
+
+class Auth(Base):
+    __tablename__ = "auths"
+
+    id = Column(Integer, primary_key = True, index=True)
