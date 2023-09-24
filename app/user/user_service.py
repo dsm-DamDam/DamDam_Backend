6dd30c5bbf47d8d000ec7549cdb67ac1a2fd8df1
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:41:03 +0900

    user-service

--- app/user/user_service.py
+++ app/user/user_service.py
@@ -1,9 +1,30 @@
-from database.models import User
+from database.models import User, Auth
+from passlib.context import CryptContext
 from database.session import SessionLocal
 
-def generate_verification_code():
-    # 여기에서 이메일 인증 코드를 생성하는 로직을 작성
-    pass
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
-def get_user_by_id(db: SessionLocal, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+def get_user_by_id(db: SessionLocal, id: int):
+    return db.query(User).filter(User.id == id).first()
+
+def create_user(db:SessionLocal ,auth_code : int , email : str, userID : str, password : str, nickname : str):
+    new_user = User(id = auth_code ,email=email, userID=userID, password=password, nickname=nickname)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def hash_password(password):
+    return bcrypt_context.hash(password)
+
+def get_user_by_userID(db:SessionLocal, userID : str):
+    return db.query(User).filter(User.userID == userID).first()
+
+def get_user_by_auth_code(db:SessionLocal, auth_code : int):
+    return db.query(User).filter(User.id == auth_code).first()
+
+def get_user_by_email(db:SessionLocal, email : str):
+    return db.query(User).filter(User.email == email).first()
+
+def find_auth_code(db:SessionLocal, auth_code: int):
+    return db.query(Auth).filter(Auth.id == auth_code).first()
