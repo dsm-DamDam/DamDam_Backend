fcc28fc0c42fdf722bf2b54ffae47f84fb5fc5b8
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:41:53 +0900

    item-service

---
+++ app/item/item_service.py
@@ -0,0 +1,9 @@
+from database.models import Count
+from database.session import SessionLocal
+
+def add_count(db:SessionLocal,date : int, count : int):
+    new_count_data = Count(date = date, count = 20 - count)
+    db.add(new_count_data)
+    db.commit()
+    db.refresh(new_count_data)
+    return new_count_data
