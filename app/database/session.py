9965d4019839986fabcc23f2bcb72ae9980f2563
yhd0210 <yhd060210@gmail.com>
2023-09-24 18:42:48 +0900

    DB-session

--- app/database/session.py
+++ app/database/session.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+DB_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?charset=utf8"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DB_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
