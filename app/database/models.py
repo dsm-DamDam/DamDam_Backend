11e52897c2e48cbbd0df53227513c976d219d9c7
yhd0210 <yhd060210@gmail.com>
2023-09-25 14:52:19 +0900

    DB-models

--- app/database/models.py
+++ app/database/models.py
@@ -9,27 +9,28 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-
     userID = Column(String(20), unique = True, index=True)
     email = Column(String(50), unique=True, index=True)
     nickname = Column(String(10))
     password = Column(String(30))
-    #counts = relationship("Count", back_populates="user")
-    #is_verified = Column(Boolean, default=False)
-    #verification_code = Column(String, nullable=True)
-    
+
 class Count(Base):
     __tablename__ = "counts"
 
     id = Column(Integer, primary_key=True, index=True,autoincrement=True)
-    #userID = Column(Integer, ForeignKey("users.id"))
     date = Column(Date)
     count = Column(Integer)
-
-    #user = relationship("User", back_populates="counts")
 
 
 class Auth(Base):
     __tablename__ = "auths"
 
     id = Column(Integer, primary_key = True, index=True)
+
+
+class Auth_user(Base):
+    __tablename__ = "user_auths"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))  # 사용자의 ID를 참조하는 외래 키
+    token = Column(String(255))  # 토큰 정보를 저장하는 필드
