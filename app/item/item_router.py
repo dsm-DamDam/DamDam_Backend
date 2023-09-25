e67246eca232c4675c908cdd4fc39caf89eb4c73
yhd0210 <yhd060210@gmail.com>
2023-09-25 14:52:27 +0900

    item-router

--- app/item/item_router.py
+++ app/item/item_router.py
@@ -7,17 +7,16 @@ from .schemas import HW, daily_count
 from datetime import date
 from database.models import Count
 
-
 router = APIRouter()
 
-@router.post("/get_item")
+@router.post("/get-item")
 async def get_item(item : HW, db : Session = Depends(get_db)):
     add_data = item_service.add_count(db, date.today() ,item.count)
     if add_data is None:
         raise HTTPException(status_code=400, detail="데이터가 전송이 되지 않았습니다.")
     return add_data, {"detail":"데이터가 성공적으로 전송 되었습니다."}
 
-@router.get("/daily_total_cnt")
+@router.get("/daily-total-cnt")
 async def daily_total_count( db : Session = Depends(get_db)):
     total =  db.query(func.sum(Count.count)).filter(Count.date == date.today()).scalar() or 0
     return {"total_count": total, "today":date.today()}
