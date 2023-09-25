dae717e26cca3a906e0025a8cbb0736d902ce3a9
yhd0210 <yhd060210@gmail.com>
2023-09-25 14:52:05 +0900

    security

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,15 @@
-from .config import settings
-from .security import setup_security
+# from .config import settings
+# from .security import setup_security
+import secrets
 
-# 애플리케이션 초기화
-def initialize_app():
-    # 보안 설정 초기화
-    setup_security(settings)
+# # 애플리케이션 초기화
+# def initialize_app():
+#     # 보안 설정 초기화
+#     setup_security(settings)
 
-# 애플리케이션 초기화 함수를 실행
-initialize_app()
+# # 애플리케이션 초기화 함수를 실행
+# initialize_app()
+
+SECRET_KEY = secrets.token_hex(32)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
