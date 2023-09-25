2addc614f451b35a4b34f38e3ac2781cd0bd9ad5
yhd0210 <yhd060210@gmail.com>
2023-09-25 14:51:53 +0900

    config

--- app/core/config.py
+++ app/core/config.py
@@ -8,5 +8,6 @@ class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_NAME: str = "default_database"
     DB_PORT: int = 3306
+    SECRET_KEY : str = "909889124130safa31bda1"
 
 settings = Settings()
