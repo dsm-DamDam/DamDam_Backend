4ebd36ebfe6cf8ff5d1da169a3fc7c90cb65a60c
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-11-28 15:59:47 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -37,4 +37,4 @@ async def test():
     return 'Server is Working!'
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='172.20.10.4',port=8080,reload=True)
+    uvicorn.run("main:app", host='',port=8080,reload=True)
