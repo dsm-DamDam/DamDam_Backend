4b899e96be31fd385b924ab13572541e33edebcc
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-12-02 15:11:35 +0900

    config

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,13 @@
+from decouple import config
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    DB_USER: str = config('DB_USER')
+    DB_PASSWORD: str = config('DB_PASSWORD')
+    DB_HOST: str = config('DB_HOST')
+    DB_NAME: str = config('DB_NAME')
     APP_NAME: str = "DamDam-App"
     DEBUG: bool = False
-    DB_USER: str = "default_user"
-    DB_PASSWORD: str = "default_password"
-    DB_HOST: str = "localhost"
-    DB_NAME: str = "default_database"
     DB_PORT: int = 3306
-    SECRET_KEY : str = "909889124130safa31bda1"
 
 settings = Settings()
