fbf948d94898b142b5c7b798c490d8750398d19f
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-12-02 15:11:45 +0900

    security

--- app/core/security.py
+++ app/core/security.py
@@ -1,15 +1,34 @@
-# from .config import settings
-# from .security import setup_security
+from .config import settings
 import secrets
-
-# # 애플리케이션 초기화
-# def initialize_app():
-#     # 보안 설정 초기화
-#     setup_security(settings)
-
-# # 애플리케이션 초기화 함수를 실행
-# initialize_app()
+from database.session import get_db
+from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from database.models import User
+from fastapi import Depends, HTTPException, status
 
 SECRET_KEY = secrets.token_hex(32)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def verify_token(token: str, db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id : str = payload.get("sub")
+        print(user_id)
+        if user_id is None:
+            raise credentials_exception
+    except JWTError as e:
+        print(f"JWT decode error: {e}")
+        raise credentials_exception
+    
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    print(user)
+    if user is None:
+        raise credentials_exception
+
+    return user
